4ba132ce9ccee3ce57fc95930ccafae697063335
Fube <fubeitch@gmail.com>
2020-08-29 12:40:59 -0400

    refactored uint to int

--- Program.cs
+++ Program.cs
@@ -7,8 +7,8 @@ namespace SudokuSolver
 
     class Grid
     {
-        //Why uint and not int? Idk, felt like it.
-        uint[,] data;
+
+        int[,] data;
 
         /**
          * Constructors should be self-explanatory.
@@ -16,23 +16,23 @@ namespace SudokuSolver
 
         public Grid()
         {
-            data = new uint[9, 9];
-            for (uint i = 0; i < 9; i++)
-                for (uint j = 0; j < 9; j++)
+            data = new int[9, 9];
+            for (int i = 0; i < 9; i++)
+                for (int j = 0; j < 9; j++)
                     data[i, j] = 0;
         }
 
-        public Grid(uint[,] args)
+        public Grid(int[,] args)
         {
-            data = new uint[9, 9];
-            for (uint i = 0; i < 9; i++)
-                for (uint j = 0; j < 9; j++)
+            data = new int[9, 9];
+            for (int i = 0; i < 9; i++)
+                for (int j = 0; j < 9; j++)
                     data[i, j] = args[i, j];
         }
 
-        public Grid(params uint[] args)
+        public Grid(params int[] args)
         {
-            data = new uint[9, 9];
+            data = new int[9, 9];
             for(int i = 0; i < args.Length; i++)
                 data[i / 9, i % 9] = args[i];
             
@@ -42,7 +42,7 @@ namespace SudokuSolver
          * Could change this to accept a Tuple or Struct instead of 3 arguments.
          * Not sure if that's a good idea.
          **/
-        bool isPossible(uint row, uint col, uint num)
+        bool IsPossible(int row, int col, int num)
         {
 
             /**
@@ -50,7 +50,7 @@ namespace SudokuSolver
              * For example, row = 2, col = 6 (0 indexed) will belong to the 3rd box starting from the top left.
              * As such, boxRow will be 0 and boxCol will be 6
              **/
-            uint
+            int
                 boxRow = (row / 3) * 3,
                 boxCol = (col / 3) * 3;
 
@@ -60,9 +60,9 @@ namespace SudokuSolver
              * Top left, top right, bottom left, and bottom left. In that order.
              * Pretty straightforward, right?
              **/
-            HashSet<uint> foo = new HashSet<uint>() {  data[boxRow, boxCol], data[boxRow, boxCol + 2], data[boxRow + 2, boxCol], data[boxRow + 2, boxCol + 2] };
+            HashSet<int> foo = new HashSet<int>() {  data[boxRow, boxCol], data[boxRow, boxCol + 2], data[boxRow + 2, boxCol], data[boxRow + 2, boxCol + 2] };
                         
-            for (uint i = 0; i < 9; i++)
+            for (int i = 0; i < 9; i++)
             {
                 foo.Add(data[row, i]);
                 foo.Add(data[i, col]);
@@ -72,11 +72,11 @@ namespace SudokuSolver
             return !foo.Contains(num);
         }
 
-        bool isSolved()
+        bool IsSolved()
         {
             // If the sudoku has an empty spot, it is obviously not solved.
-            foreach(uint i in Enumerable.Range(0, 9))
-                foreach(uint j in Enumerable.Range(0, 9))
+            foreach(int i in Enumerable.Range(0, 9))
+                foreach(int j in Enumerable.Range(0, 9))
                     if(data[i, j] == 0)
                         return false;
 
@@ -84,9 +84,9 @@ namespace SudokuSolver
         }
 
         // Simple backtracking. Nothing spectacular.
-        bool helper(uint row = 0, uint col = 0)
+        bool Helper(int row = 0, int col = 0)
         {
-            if (isSolved())
+            if (IsSolved())
                 return true;
 
             /**
@@ -94,25 +94,25 @@ namespace SudokuSolver
              * We will need to reset column to 0 and increment row by 1.
              * This does that.
              **/
-            uint 
+            int 
                 nextCol = (1 + col) % 9,
                 nextRow = nextCol == 0 ? row + 1 : row;
 
             // Makes sure we aren't overwriting a value.
             if (data[row, col] != 0)
-                return helper(nextRow, nextCol);
+                return Helper(nextRow, nextCol);
 
             /**
              * Why 1 to 10?
              * These are all the legal values a cell in a sudoku board can hold (range(1, 9)).
              **/
-            foreach(uint i in Enumerable.Range(1, 9))
+            foreach(int i in Enumerable.Range(1, 9))
             {
-                if (isPossible(row, col, i))
+                if (IsPossible(row, col, i))
                 {
                     data[row, col] = i;
 
-                    if (helper(nextRow, nextCol))
+                    if (Helper(nextRow, nextCol))
                         return true;
                 }
                 data[row, col] = 0;
@@ -120,11 +120,11 @@ namespace SudokuSolver
             return false;
         }
 
-        // Why not just do: Solve(uint x = 0, uint y = 0)?
+        // Why not just do: Solve(int x = 0, int y = 0)?
         // So that the user can't do: Solve(1, 5)
         public bool Solve()
         {
-            return helper();
+            return Helper();
         }
 
         public override string ToString()
@@ -132,12 +132,12 @@ namespace SudokuSolver
 
             string toPrint = "";
 
-            for (uint i = 0; i < 9; i++)
+            for (int i = 0; i < 9; i++)
             {
                 if (i % 3 == 0)
                     toPrint += "\n";
                 toPrint += "\t| ";
-                for (uint j = 0; j < 9; j++)
+                for (int j = 0; j < 9; j++)
                 {
                     if (j % 3 == 0 && j > 0)
                         toPrint += " | ";
@@ -159,7 +159,7 @@ namespace SudokuSolver
              * It is currently 12:48 AM. This is as good as it gets for now.
              * Btw, 0 means that it is an empty value.
              **/
-            Grid sudoku = new Grid(new uint[] {
+            Grid sudoku = new Grid(new int[] {
                 0, 0, 0,
                 2, 6, 0,
                 7, 0, 1,
