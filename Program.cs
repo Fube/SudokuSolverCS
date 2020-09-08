88fd6f61e262c1a5be072fcda646b0c666dea1cb
Fube <fubeitch@gmail.com>
2020-09-08 17:18:10 -0400

    Made no change

--- Program.cs
+++ Program.cs
@@ -160,7 +160,7 @@ namespace SudokuSolver
              * Btw, 0 means that it is an empty value.
              **/
             Grid sudoku = new Grid(new int[] {
-                9, 0, 5,
+                0, 0, 0,
                 2, 6, 0,
                 7, 0, 1,
 
