fc5077cc4f19e7b05195105770efb1f3396c7694
Jean-Christophe Benoît <jc_ben@live.ca>
2020-09-08 17:18:57 -0400

    changed a num on 162 now

--- Program.cs
+++ Program.cs
@@ -160,7 +160,7 @@ namespace SudokuSolver
              * Btw, 0 means that it is an empty value.
              **/
             Grid sudodu = new Grid(new int[] {
-                0, 0, 0,
+                0, 0, 69,
                 2, 6, 0,
                 7, 0, 1,
 
